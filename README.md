810786d003c98d1c8abeb4cdff078da16043ae9c
gfalco <microsolco@gmail.com>
2015-11-16 09:44:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Getting and Cleaning Data on Coursera: Course Project
+
+=========================================
+
+
+
+
+The raw data
+
+------------------
+
+
+The 561 features are unlabeled, as in the x_test.txt file. 
+The activity labels are in the y_test.txt file.
+
+The test subjects are in the subject_test.txt file.
+
+
+The same holds for the training set.
+
+
+Run_Analysis script and the tidy dataset
+
+-------------------------------------
+
+The script called run_analysis.R is what I created to obtain the tidy dataset. It will merge the test and training sets together.
+
+Prerequisites for this script:
+
+
+..1. the UCI HAR Dataset must be extracted and
+.
+.2. the UCI HAR Dataset must be availble in a directory called "UCI HAR Dataset"
+
+
+After merging testing and training, labels are added and only columns that have to do with mean and standard deviation are kept.
+
+Lastly, the script will create a tidy data set containing the means of all the columns per test subject and per activity.
+This tidy dataset will be written to a tab-delimited file called tidy.txt, which can also be found in this repository.
+
+
---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+#Initialization, load data
+setwd("~/gacd/")
+training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
+training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+
+testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
+testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
+testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+activities = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+
+# Read features and modify feature names 
+features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+features[,2] = gsub('-mean', 'Mean', features[,2])
+features[,2] = gsub('-std', 'Std', features[,2])
+features[,2] = gsub('[-()]', '', features[,2])
+
+# Merge training and test sets 
+allData = rbind(training, testing)
+
+# Get only the data on mean and std. dev.
+cols <- grep(".*Mean.*|.*Std.*", features[,2])
+# First reduce the features table to what we want
+features <- features[cols,]
+# add the columns "subject" "activity"
+cols <- c(cols, 562, 563)
+# remove unwanted columns from allData
+allData <- allData[,cols]
+# Add the features names to allData
+colnames(allData) <- c(features$V2, "Activity", "Subject")
+colnames(allData) <- tolower(colnames(allData))
+
+currentActivity = 1
+for (currentActivityLabel in activities$V2) {
+        allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
+        currentActivity <- currentActivity + 1
+}
+
+allData$activity <- as.factor(allData$activity)
+allData$subject <- as.factor(allData$subject)
+
+tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
+# Remove the subject and activity column, since a mean of those has no use
+tidy[,90] = NULL
+tidy[,89] = NULL
+write.table(tidy, "tidy.txt", sep="\t")
+
